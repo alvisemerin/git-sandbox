58b4502a6b463fad4179c10fb133be8d1aa181e2
Alvis jobi <alvisemerinjo7777@gmail.com>
2026-08-14 00:43:56 +0530

    My very first code commit

---
+++ index.java
@@ -0,0 +1,9 @@
+public class index
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My new Project");
+        System.out.println("Hello, " + args[0]);
+
+    }    
+}
