c1f8ac1fec0a682cfbf933eb53b469fd3680dfec
Alvis jobi <alvisemerinjo7777@gmail.com>
2026-08-15 10:47:04 +0530

    new_reg

--- index.java
+++ index.java
@@ -1,9 +1,15 @@
 public class index
-{
+{ //main class initialization
     public static void main(String[] args)
-    {
+    { //main class has been initialized
         System.out.println("My new Project");
         System.out.println("Hello, " + args[0]);
-
-    }    
+        int i;
+        i=1;
+        while (i<10)
+        {
+            System.out.println("This is an infinite loop,... BUt has End condition:  "+i);
+            i++;
+        }    
+    }
 }
